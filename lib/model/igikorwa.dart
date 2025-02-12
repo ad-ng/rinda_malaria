80fbd9309b4472d683ec598cff4c371c22c61f92
ad-ng <adolphengoga@gmail.com>
2025-02-12 06:44:51 +0200

    adding a model

---
+++ lib/model/igikorwa.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Igikorwa {
+  String umuti;
+  int ibiro;
+  int imyaka;
+  String ingano;
+  Igikorwa({
+    required this.umuti,
+    required this.ibiro,
+    required this.imyaka,
+    required this.ingano,
+  });
+
+  gushakaDose() {}
+}
