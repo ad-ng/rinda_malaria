ffc75bcd8e7914fe4fa8c95b3e5826bf82a4fa10
ad-ng <adolphengoga@gmail.com>
2025-03-01 08:34:42 +0200

    adding select type of malaria page

--- lib/pages/page_2.dart
+++ lib/pages/page_2.dart
@@ -6,193 +6,108 @@ import 'package:rinda_malaria/components/backButton.dart';
 import 'package:rinda_malaria/components/headers.dart';
 import 'package:rinda_malaria/components/mybutton.dart';
 import 'package:rinda_malaria/model/igikorwa.dart';
-import 'package:rinda_malaria/pages/page_4.dart';
+import 'package:rinda_malaria/pages/page_3.dart';
+import 'package:rinda_malaria/pages/page_5.dart';
 
 class Page2 extends StatefulWidget {
-  const Page2({super.key});
+  Page2({
+    super.key,
+  });
 
   @override
-  State<Page2> createState() => _Page2State();
+  State<Page2> createState() => _Page3State();
 }
 
-List<String> options = [
-  'Artemether Lumefantrine',
-  'DihydroartemisininPiperaquine',
-  'Artesunate -Pyronaridine'
-];
+List<String> options = ['Simple Malaria', 'Simple Malaria + MDS', 'Severe'];
 
-class _Page2State extends State<Page2> {
+class _Page3State extends State<Page2> {
   String currentOption = options[0];
-
-  options_weigh() {
-    if (currentOption == 'Artemether Lumefantrine') {
-      return [
-        'Ibiro 5 - <15',
-        'Ibiro 15 - <25',
-        'Ibiro 25 - <35',
-        'Ibiro 35 no kuzamura'
-      ];
-    }
-    if (currentOption == 'Artesunate -Pyronaridine') {
-      return [
-        'Ibiro 5- < 8',
-        'Ibiro 8- < 15',
-        'Ibiro 15- <20',
-        'Ibiro 20- <24',
-        'Ibiro 24- <45',
-        'Ibiro 45 <65',
-        'Ibiro 65 kuzamura'
-      ];
-    }
-    if (currentOption == 'DihydroartemisininPiperaquine') {
-      return [
-        'Ibiro 5 - < 8',
-        'Ibiro 8- <11',
-        'Ibiro 11- <17',
-        'Ibiro 17 - <25',
-        'Ibiro 25 - <36',
-        'Ibiro 36 - <60',
-        'Ibiro 60 - <80',
-        'Ibiro 80 Kuzamura'
-      ];
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
-    return Consumer<Igikorwa>(
-      builder: (context, value, child) {
-        value.umuti = currentOption;
-        return Scaffold(
-          body: SafeArea(
-            child: Stack(
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Image.asset(
+              '././lib/images/transmission.jpg',
+              fit: BoxFit.cover,
+              height: screenHeight,
+            ),
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 3,
+                sigmaY: 2,
+              ),
+              child: Container(
+                color: Colors.black.withOpacity(
+                    0), // Optional: overlay to make it darker or transparent
+              ),
+            ),
+            Column(
               children: [
-                Image.asset(
-                  '././lib/images/transmission.jpg',
-                  fit: BoxFit.cover,
-                  height: screenHeight,
+                BackButtonCustom(
+                  isLast: false,
                 ),
-                BackdropFilter(
-                  filter: ImageFilter.blur(
-                    sigmaX: 3,
-                    sigmaY: 2,
-                  ),
-                  child: Container(
-                    color: Colors.black.withOpacity(
-                        0), // Optional: overlay to make it darker or transparent
+                Headers(),
+                SizedBox(
+                  height: screenHeight * 0.000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Ubwoko bwa Maraliya',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
                   ),
                 ),
-                Column(
-                  children: [
-                    BackButtonCustom(
-                      isLast: false,
-                    ),
-                    Headers(),
-                    SizedBox(
-                      height: screenHeight * 0.05,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Hitamo umuti',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black,
-                        ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Card(
+                      color: Colors.white,
+                      child: ListView.builder(
+                        itemCount: options.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(
+                              options[index],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 20),
+                            ),
+                            leading: Radio(
+                              activeColor: Colors.black,
+                              value: options[index],
+                              groupValue: currentOption,
+                              onChanged: (value) {
+                                setState(() {
+                                  currentOption = value.toString();
+                                });
+                              },
+                            ),
+                          );
+                        },
                       ),
                     ),
-                    SizedBox(
-                      height: screenHeight * 0.01,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        color: Colors.white,
-                        child: Column(
-                          children: [
-                            ListTile(
-                              title: Text(
-                                'Artemether Lumefantrine',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.black,
-                                    fontSize: 20),
-                              ),
-                              trailing: Image.asset(
-                                '././lib/images/Antimalarial-Artemether-Lumefantrine-20-120mg.jpg',
-                                height: screenHeight * 0.1,
-                              ),
-                              leading: Radio(
-                                activeColor: Colors.black,
-                                value: options[0],
-                                groupValue: currentOption,
-                                onChanged: (value) {
-                                  setState(() {
-                                    currentOption = value.toString();
-                                  });
-                                },
-                              ),
-                            ),
-                            ListTile(
-                              title: Text('Dihydroartemisinin - Piperaquine',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black,
-                                      fontSize: 20)),
-                              trailing: Image.asset(
-                                '././lib/images/dihydroartemisinin-piperaquine-tablets.jpg',
-                                height: screenHeight * 0.1,
-                              ),
-                              leading: Radio(
-                                activeColor: Colors.black,
-                                value: options[1],
-                                groupValue: currentOption,
-                                onChanged: (value) {
-                                  setState(() {
-                                    currentOption = value.toString();
-                                  });
-                                },
-                              ),
-                            ),
-                            ListTile(
-                              title: Text('Artesunate -Pyronaridine',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black,
-                                      fontSize: 20)),
-                              trailing: Image.asset(
-                                '././lib/images/artesunate-tablets-box.png',
-                                height: screenHeight * 0.1,
-                              ),
-                              leading: Radio(
-                                activeColor: Colors.black,
-                                value: options[2],
-                                groupValue: currentOption,
-                                onChanged: (valuen) {
-                                  setState(() {
-                                    currentOption = valuen.toString();
-                                  });
-                                },
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    MyButton(
-                      page: Page4(
-                          options_weigh: options_weigh(),
-                          currentOption_weigh: options_weigh()[0]),
-                    )
-                  ],
+                  ),
                 ),
+                MyButton(page: Page3()),
+                SizedBox(
+                  height: screenHeight * 0.08,
+                )
               ],
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/page_3.dart
@@ -0,0 +1,198 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rinda_malaria/components/backButton.dart';
+import 'package:rinda_malaria/components/headers.dart';
+import 'package:rinda_malaria/components/mybutton.dart';
+import 'package:rinda_malaria/model/igikorwa.dart';
+import 'package:rinda_malaria/pages/page_4.dart';
+
+class Page3 extends StatefulWidget {
+  const Page3({super.key});
+
+  @override
+  State<Page3> createState() => _Page3State();
+}
+
+List<String> options = [
+  'Artemether Lumefantrine',
+  'DihydroartemisininPiperaquine',
+  'Artesunate -Pyronaridine'
+];
+
+class _Page3State extends State<Page3> {
+  String currentOption = options[0];
+
+  options_weigh() {
+    if (currentOption == 'Artemether Lumefantrine') {
+      return [
+        'Ibiro 5 - <15',
+        'Ibiro 15 - <25',
+        'Ibiro 25 - <35',
+        'Ibiro 35 no kuzamura'
+      ];
+    }
+    if (currentOption == 'Artesunate -Pyronaridine') {
+      return [
+        'Ibiro 5- < 8',
+        'Ibiro 8- < 15',
+        'Ibiro 15- <20',
+        'Ibiro 20- <24',
+        'Ibiro 24- <45',
+        'Ibiro 45 <65',
+        'Ibiro 65 kuzamura'
+      ];
+    }
+    if (currentOption == 'DihydroartemisininPiperaquine') {
+      return [
+        'Ibiro 5 - < 8',
+        'Ibiro 8- <11',
+        'Ibiro 11- <17',
+        'Ibiro 17 - <25',
+        'Ibiro 25 - <36',
+        'Ibiro 36 - <60',
+        'Ibiro 60 - <80',
+        'Ibiro 80 Kuzamura'
+      ];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Consumer<Igikorwa>(
+      builder: (context, value, child) {
+        value.umuti = currentOption;
+        return Scaffold(
+          body: SafeArea(
+            child: Stack(
+              children: [
+                Image.asset(
+                  '././lib/images/transmission.jpg',
+                  fit: BoxFit.cover,
+                  height: screenHeight,
+                ),
+                BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 3,
+                    sigmaY: 2,
+                  ),
+                  child: Container(
+                    color: Colors.black.withOpacity(
+                        0), // Optional: overlay to make it darker or transparent
+                  ),
+                ),
+                Column(
+                  children: [
+                    BackButtonCustom(
+                      isLast: false,
+                    ),
+                    Headers(),
+                    SizedBox(
+                      height: screenHeight * 0.05,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Hitamo umuti',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.01,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'Artemether Lumefantrine',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black,
+                                    fontSize: 20),
+                              ),
+                              trailing: Image.asset(
+                                '././lib/images/Antimalarial-Artemether-Lumefantrine-20-120mg.jpg',
+                                height: screenHeight * 0.1,
+                              ),
+                              leading: Radio(
+                                activeColor: Colors.black,
+                                value: options[0],
+                                groupValue: currentOption,
+                                onChanged: (value) {
+                                  setState(() {
+                                    currentOption = value.toString();
+                                  });
+                                },
+                              ),
+                            ),
+                            ListTile(
+                              title: Text('Dihydroartemisinin - Piperaquine',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black,
+                                      fontSize: 20)),
+                              trailing: Image.asset(
+                                '././lib/images/dihydroartemisinin-piperaquine-tablets.jpg',
+                                height: screenHeight * 0.1,
+                              ),
+                              leading: Radio(
+                                activeColor: Colors.black,
+                                value: options[1],
+                                groupValue: currentOption,
+                                onChanged: (value) {
+                                  setState(() {
+                                    currentOption = value.toString();
+                                  });
+                                },
+                              ),
+                            ),
+                            ListTile(
+                              title: Text('Artesunate -Pyronaridine',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black,
+                                      fontSize: 20)),
+                              trailing: Image.asset(
+                                '././lib/images/artesunate-tablets-box.png',
+                                height: screenHeight * 0.1,
+                              ),
+                              leading: Radio(
+                                activeColor: Colors.black,
+                                value: options[2],
+                                groupValue: currentOption,
+                                onChanged: (valuen) {
+                                  setState(() {
+                                    currentOption = valuen.toString();
+                                  });
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    MyButton(
+                      page: Page4(
+                          options_weigh: options_weigh(),
+                          currentOption_weigh: options_weigh()[0]),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
