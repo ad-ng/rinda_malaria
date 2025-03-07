ce89fe4903e71d0dc0bdf31832d1c6ebc8f29f4b
ad-ng <adolphengoga@gmail.com>
2025-03-07 08:23:46 +0200

    adding tables

--- lib/pages/page_8.dart
+++ lib/pages/page_8.dart
@@ -11,7 +11,7 @@ import 'package:rinda_malaria/pages/page_9.dart';
 class Page8 extends StatefulWidget {
   final List<String> options_weigh;
   String currentOption_weigh;
-   Page8({
+  Page8({
     super.key,
     required this.options_weigh,
     required this.currentOption_weigh,
@@ -25,6 +25,376 @@ class _Page8State extends State<Page8> {
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
+    selectTable(String umuti) {
+      if (umuti == 'AL') {
+        return SizedBox(
+          width: 500,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.white,
+              child: Table(
+                columnWidths: {
+                  0: FractionColumnWidth(0.12),
+                  1: FractionColumnWidth(0.18),
+                  2: FractionColumnWidth(0.62)
+                },
+                border: TableBorder.all(),
+                children: [
+                  TableRow(
+                    children: [
+                      Text(
+                        'Body Weight in Kg',
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        'Artemether (20mg) + Lumefantrine (120mg) Blisters',
+                        textAlign: TextAlign.center,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'Number of Tablets of AL per Dose',
+                            textAlign: TextAlign.center,
+                          ),
+                          Container(
+                            child: Table(
+                              border: TableBorder.all(),
+                              children: [
+                                TableRow(
+                                  children: [
+                                    Text(
+                                      'Day 1',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      'Day 2',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      'Day 3',
+                                      textAlign: TextAlign.center,
+                                    )
+                                  ],
+                                ),
+                                TableRow(
+                                  children: [
+                                    Container(
+                                      child: Table(
+                                        border: TableBorder.all(),
+                                        children: [
+                                          TableRow(
+                                            children: [
+                                              Text(
+                                                'First Dose',
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              Text(
+                                                '8 \nHours After',
+                                                textAlign: TextAlign.center,
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Table(
+                                        border: TableBorder.all(),
+                                        children: [
+                                          TableRow(
+                                            children: [
+                                              Text(
+                                                '24 \nHours After',
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              Text(
+                                                '36 \nHours After',
+                                                textAlign: TextAlign.center,
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Table(
+                                        border: TableBorder.all(),
+                                        children: [
+                                          TableRow(
+                                            children: [
+                                              Text(
+                                                '48 \nHours After',
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              Text(
+                                                '60 \nHours After',
+                                                textAlign: TextAlign.center,
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Text(
+                        '< 15 kg',
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '6x1',
+                        textAlign: TextAlign.center,
+                      ),
+                      Container(
+                        child: Table(
+                          border: TableBorder.all(),
+                          children: [
+                            TableRow(
+                              children: [
+                                Text(
+                                  '1\n ',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '1',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '1',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '1',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '1',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '1',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Text(
+                        '15 kg to< 25 kg',
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '6x2',
+                        textAlign: TextAlign.center,
+                      ),
+                      Container(
+                        child: Table(
+                          border: TableBorder.all(),
+                          children: [
+                            TableRow(
+                              children: [
+                                Text(
+                                  '2\n ',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '2',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '2',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '2',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '2',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '2',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Text(
+                        '25 kg to < 35 kg',
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '6x3',
+                        textAlign: TextAlign.center,
+                      ),
+                      Container(
+                        child: Table(
+                          border: TableBorder.all(),
+                          children: [
+                            TableRow(
+                              children: [
+                                Text(
+                                  '3\n',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '3',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '3',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '3',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '3',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '3',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Text(
+                        '≥ 35 kg',
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '6x4',
+                        textAlign: TextAlign.center,
+                      ),
+                      Container(
+                        child: Table(
+                          border: TableBorder.all(),
+                          children: [
+                            TableRow(
+                              children: [
+                                Text(
+                                  '4\n',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '4',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '4',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '4',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '4',
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '4',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      } else if (umuti == 'ASPY') {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Table(
+            border: TableBorder.all(),
+            children: [
+              TableRow(
+                children: [
+                  Text('Formulations Available'),
+                  Text('Body Weight (kg)'),
+                  Text('Dosage'),
+                  Text('Prescription'),
+                ],
+              ),
+              TableRow(
+                children: [
+                  Text('Oral Suspension of Granules'),
+                  Container(
+                    child: Table(
+                      border: TableBorder.all(),
+                      children: [
+                        TableRow(
+                          children: [
+                            Text('≥ 5 to <8'),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Text('data'),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Text('data'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Text('data'),
+                  Text('data'),
+                ],
+              ),
+              TableRow(
+                children: [
+                  Text('Tablets'),
+                  Text('data'),
+                  Text('data'),
+                  Text('data'),
+                ],
+              )
+            ],
+          ),
+        );
+      }
+    }
+
     return Consumer<Igikorwa>(
       builder: (context, value, child) {
         value.ibiro = widget.currentOption_weigh;
@@ -71,36 +441,13 @@ class _Page8State extends State<Page8> {
                       height: screenHeight * 0.01,
                     ),
                     Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Card(
-                          color: Colors.white,
-                          child: ListView.builder(
-                            itemCount: widget.options_weigh.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(
-                                  widget.options_weigh[index],
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 20),
-                                ),
-                                leading: Radio(
-                                  activeColor: Colors.black,
-                                  value: widget.options_weigh[index],
-                                  groupValue: widget.currentOption_weigh,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      widget.currentOption_weigh =
-                                          value.toString();
-                                    });
-                                  },
-                                ),
-                              );
-                            },
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          SizedBox(
+                            child: selectTable('ASPY'),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                     MyButton(page: Page9()),
@@ -116,4 +463,13 @@ class _Page8State extends State<Page8> {
       },
     );
   }
+
+  TableRow buildRow(List<String> cells) => TableRow(
+        children: cells.map((cell) {
+          return Padding(
+            padding: const EdgeInsets.all(12),
+            child: Center(child: Text(cell)),
+          );
+        }).toList(),
+      );
 }
